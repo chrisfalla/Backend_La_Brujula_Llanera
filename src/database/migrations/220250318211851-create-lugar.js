cf55e653fb66981943b27991d3738bab5dca2e23
chrisfalla <christoferfallavillarreal@gmail.com>
2025-03-19 18:50:50 -0500

    base de datos migrada a supabase

---
+++ src/database/migrations/220250318211851-create-lugar.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+export const up = async (queryInterface, Sequelize) => {
+  await queryInterface.createTable('Lugares', {
+    id_Lugar: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    nombre_Lugar: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    descripcion: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    },
+    direccion: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    estrellas: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    }
+  });
+};
+
+export const down = async (queryInterface, Sequelize) => {
+  await queryInterface.dropTable('Lugares');
+};
