ef4d8baddb4ee50516b54bc61abad0719f017edc
chrisfalla <christoferfallavillarreal@gmail.com>
2025-04-10 20:21:35 -0500

    Import and Calling
    importation of dependeces with routes

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import process from 'node:process';
 import router from './src/routes/UserRoute.js';
 import authRouter from './src/routes/AuthRoute.js';
 import postRouter from './src/routes/postsRoute.js';
 import swaggerSpec from './swagger/swaggerConfig.js';
 import swaggerUi from 'swagger-ui-express';
+import categoryRouter from './src/routes/CategoryRoute.js'; // Importa correctamente el archivo de rutas
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use("/api", router);
-app.use("/auth", authRouter);    // login
-app.use("/posts", postRouter);   // posts protegidos
+app.use("/api", router); // Usuarios
+app.use("/auth", authRouter); // Login
+app.use("/posts", postRouter); // Posts protegidos
+app.use("/categories", categoryRouter); // ✅ Rutas de categorías correctamente registradas
 
 // Ruta base
 app.get('/', (req, res) => {
