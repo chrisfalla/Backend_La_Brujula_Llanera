f7b17fa1d29f6f835512c246293edcc644423dff
StevenBalagueraP <161085601+StevenBalagueraP@users.noreply.github.com>
2025-05-13 15:58:19 -0500

    fixing trouble about the tagDTO response

--- src/application/DTOs/PromotedPlacesByTagDTO.js
+++ src/application/DTOs/PromotedPlacesByTagDTO.js
@@ -1,8 +1,8 @@
 export default class RatingStarsByTagsDTO {
-    constructor(placeId ,placeName, placeAddess, imageUrl, imageCategoryName, ratingStars) {
+    constructor(placeId ,placeName, placeAddress, imageUrl, imageCategoryName, ratingStars) {
         this.idPlace = placeId;
         this.placeName = placeName;
-        this.placeAddess = placeAddess;
+        this.placeAddress = placeAddress;
         this.imageUrl = imageUrl;
         this.imageCategoryName = imageCategoryName;
         this.ratingStars = ratingStars;
