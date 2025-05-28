b450b0dec483f4e9cb89d583bbfb2c7a750b6b6d
StevenBalagueraP <161085601+StevenBalagueraP@users.noreply.github.com>
2025-05-27 21:35:27 -0500

    feat: :zap: Adding PlacesByCategoryEndpoint into placeDetails
    I added PlacesByCategory and I injected all the dependencies on server.js
    
    PlacesByCategoryEndpoint

--- src/interfaces/controllers/PlaceDetailsController.js
+++ src/interfaces/controllers/PlaceDetailsController.js
@@ -1,6 +1,7 @@
 export default class PlaceDetailsController {
-    constructor(placeDetailUseCase) {
+    constructor(placeDetailUseCase, getPlacesByCategoryUseCase) {
         this.placeDetailUseCase = placeDetailUseCase;
+        this.getPlacesByCategoryUseCase = getPlacesByCategoryUseCase;
     }
 
     async getPlaceDetails(req, res) {
@@ -15,6 +16,19 @@ export default class PlaceDetailsController {
             return res.status(200).json(placeDetails);
         } catch (error) {
             console.error("Error fetching place details:", error);
+            return res.status(500).json({ message: "Internal server error" });
+        }
+    }
+    async getPlacesByCategory(req, res) {
+        try {
+            const { idCategory } = req.params; 
+            const places = await this.getPlacesByCategoryUseCase.getPlacesByCategory(idCategory);
+            if (!places || places.length === 0) {
+                return res.status(404).json({ message: "No places found for this category" });
+            }
+            return res.status(200).json(places);
+        } catch (error) {
+            console.error("Error fetching places by category:", error);
             return res.status(500).json({ message: "Internal server error" });
         }
     }
